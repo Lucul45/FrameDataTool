dc169c160db724bb0dc6f0cd6cbc5e854d403c6f
Lucul45 <71066283+Lucul45@users.noreply.github.com>
2025-07-25 20:27:45 +0200

    remove direct ref in attack detection

--- Assets/Scripts/AttackDetection.cs
+++ Assets/Scripts/AttackDetection.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class AttackDetection : MonoBehaviour
 {
-    [SerializeField] private PlayerController _p1;
-    [SerializeField] private PlayerController _p2;
-    [SerializeField] private BoxCollider2D _attackCollider;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GetComponentInParent<PlayerController>().IsHitting = true;
         if (collision.tag == "Player2")
         {
-            _p1.IsHitting = true;
             PlayerStateMachineManager.Instance.ChangeStateP2(EPlayerState.HURT);
             PlayerStateMachineManager.Instance.CurrentStateP2.AttackHitten = GetComponentInParent<PlayerController>().CurrentAttack;
         }
         else if (collision.tag == "Player1")
         {
-            _p2.IsHitting = true;
             PlayerStateMachineManager.Instance.ChangeStateP1(EPlayerState.HURT);
             PlayerStateMachineManager.Instance.CurrentStateP1.AttackHitten = GetComponentInParent<PlayerController>().CurrentAttack;
         }
